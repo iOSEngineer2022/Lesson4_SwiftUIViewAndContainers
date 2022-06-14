c3d08da5c06fb7db0364aa296008717102ed8c10
Yüksel Toprak <yuekseltoprak@mailbox.org>
2022-06-14 14:53:50 +0200

    Initial Commit

---
+++ Lesson4_SwiftUIViewAndContainers/Lesson4_SwiftUIViewAndContainersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson4_SwiftUIViewAndContainersApp.swift
+//  Lesson4_SwiftUIViewAndContainers
+//
+//  Created by Yüksel Toprak on 14.06.22.
+//
+
+import SwiftUI
+
+@main
+struct Lesson4_SwiftUIViewAndContainersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
